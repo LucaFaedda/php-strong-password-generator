9a2df61fc9d54f1c88be6182efb3fa706ef11f76
LucaFaedda <lucakarl91@gmail.com>
2023-01-31 14:50:41 +0100

    scritto struttura generale con boostrap

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php 
+
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <title>Password Generator</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center">
+                <h1>Strong Password Generator</h1>
+                <h2>Genera una password sicura</h2>
+            </div>
+        </div>
+        <div class="col-12 mt-5">
+            <form action="./index.php" method="GET">
+                    <input type="number" name="psw" >
+                    <button type="submit" class="btn btn-primary">Genera</button>
+            </form>
+        </div>
+    </div>
+    
+</body>
+</html>
