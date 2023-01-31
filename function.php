fa9f32da2003c2d1165d2a088ddfbefb47080498
LucaFaedda <lucakarl91@gmail.com>
2023-01-31 16:29:58 +0100

    aggiunto codici errori

--- function.php
+++ function.php
@@ -1,5 +1,9 @@
 <?php 
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 function generatePsw($lunghezza)
 {
     $valori = 'abcdefghijklmnopqurstuvwyzABCDEFGHIJKLMNOPQURSTUVWYZ0123456789!?%&';
